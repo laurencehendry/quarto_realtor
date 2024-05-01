53388c67761a10916ed2895d3694274c8a354ed4
laurencehendry <lmghendry@gmail.com>
2024-05-01 12:49:29 +1000

    initial edits using rvest

--- scraper_functions.R
+++ scraper_functions.R
@@ -1,5 +1,15 @@
+
+
+library(pagedown)
+library(htmltools)
+library(tidyverse)
+library(rvest)
+library(xml2)
+
+
+
 ####################################################
-## Functions
+## Functions - original
 ####################################################
 
 price <- function(realtor, url, element){
@@ -21,7 +31,7 @@ address_price <- function(realtor, url, price_element, address_element){
   
   remDr$navigate(url = row$url)
   
-  Sys.sleep(3)
+  Sys.sleep(5)
   
   #address
   address <- remDr$getPageSource(header = TRUE)[[1]] %>% 
@@ -65,3 +75,55 @@ address_price <- function(realtor, url, price_element, address_element){
 #                                if(nrow(df) <1){"FALSE"}
 #                                else {"TRUE"}
 #}
+
+####################################################
+## Functions - rvest
+####################################################
+
+
+address_price_rvest <- function(realtor, url, element_price, element_address){
+  # Initialize address_price object
+  address_price <- NULL
+  
+  # Error handling
+  tryCatch({
+    sess <- rvest::read_html_live(url)
+    
+    # Extract price data
+    price <- sess %>% 
+      html_elements(element_price) %>%
+      xml_text(.) %>%
+      map_chr(~as.character(as.numeric(gsub("[^0-9.]", "", .)))) %>%
+      map(~as.numeric(.x)) %>%
+      unlist(.) %>%
+      .[which(!is.na(.))] %>%
+      as.data.frame(., stringAsFactors = TRUE) %>%
+      rename("price" = ".") %>% 
+      mutate(index = row_number())
+    
+    if (nrow(price) == 0) {
+      price <- tibble(price = numeric(), index = numeric())
+    }
+    
+    # Extract address data
+    address <- sess %>% 
+      html_elements(element_address) %>%
+      xml_text(.) %>%
+      .[which(!is.na(.))] %>%
+      as.data.frame(., stringAsFactors = TRUE) %>%
+      rename("address" = ".") %>%
+      mutate_all(~ gsub("\n", "", .)) %>%
+      mutate(index = row_number())
+    
+    # Perform a full join on the 'index' column
+    address_price <- full_join(address, price, by = "index") %>%
+      mutate(realtor = realtor) %>% 
+      select(-index) %>%
+      mutate(Timestamp = Sys.time())
+    
+  }, error = function(e) {
+    # Handle errors gracefully
+    warning("An error occurred: ", conditionMessage(e))
+  })
+  return(address_price)
+}
