039536b3d2f25d93ab04da9eef758ec437d9ab40
laurencehendry <lmghendry@gmail.com>
2024-05-26 12:49:57 +1000

    latest update end-May

--- scraper_functions.R
+++ scraper_functions.R
@@ -1,12 +1,11 @@
 
-
-library(pagedown)
-library(htmltools)
-library(tidyverse)
-library(rvest)
-library(xml2)
-
-
+pacman::p_load(pagedown,
+               htmltools,
+               tidyverse,
+               rvest,
+               xml2,
+               here,
+               httr)
 
 ####################################################
 ## Functions - original
@@ -24,8 +23,6 @@ price <- function(realtor, url, element){
     as.data.frame(,stringAsFactors = TRUE) %>%
     rename(, "{{realtor}}" :="Freq")
 }
-
-
 
 address_price <- function(realtor, url, price_element, address_element){
   
@@ -126,4 +123,103 @@ address_price_rvest <- function(realtor, url, element_price, element_address){
     warning("An error occurred: ", conditionMessage(e))
   })
   return(address_price)
+}
+
+
+####################################################
+## Functions - utilities
+####################################################
+
+
+download_webpage <- function(realtor, 
+                             url) {
+  # Read realtors metadata from csv file
+  #meta <- read.csv(paste0(here::here(),"/realtors_meta.csv"))
+  
+  # URL of the webpage you want to download (using the first URL from the metadata)
+  # url <- meta %>%
+  #   filter(realtor == realtor_name,
+  #          type == buying_leasing) |>
+  #   pull(url)
+  # #meta$url[1]
+  
+  # Create a sub-folder named by today's date
+  today_date <- format(Sys.Date(), "%Y-%m-%d")
+  sub_folder <- paste0(here::here(),"/page_dumps/",today_date)
+  if (!dir.exists(sub_folder)) {
+    dir.create(sub_folder)
+  }
+  
+  # Define the file name with realtor's name and timestamp down to the minute
+  timestamp <- format(Sys.time(), "%Y-%m-%d_%H-%M")
+  file_name <- paste0(realtor, "_", timestamp, ".html")
+  
+  # Define the file path where you want to save the downloaded HTML in the sub-folder
+  file_path <- paste0(sub_folder, "/", file_name)
+  
+  # Use rvest to download the webpage
+  webpage <- read_html(url)
+  
+  # Save the HTML content to a file
+  write_html(webpage, file_path)
+  
+  # Read the contents of the HTML file into R
+  html_content <- readLines(file_path, warn = FALSE)
+  
+  # Concatenate the lines into a single character string
+  html_content <- paste(html_content, collapse = "\n")
+  
+  # Print the HTML content
+  print(html_content)
+}
+
+
+####################################################
+## Functions - ChatGPT
+####################################################
+
+# Calls the ChatGPT API with the given prompt and returns the answer
+ask_chatgpt <- function(prompt) {
+  response <- POST(
+    url = "https://api.openai.com/v1/chat/completions", #"https://api.openai.com/v1/completions", 
+    add_headers(Authorization = paste("Bearer", keyring::key_get("chatgpt"))),
+    content_type_json(),
+    encode = "json",
+    body = list(
+      model = "gpt-3.5-turbo",
+      messages = list(list(
+        role = "user", 
+        content = prompt
+      ))
+    )
+  )
+  str_trim(content(response)$choices[[1]]$message$content)
+}
+
+# name <- "Belle"
+# file_location <- scrape_list[1]
+
+extract_information <- function(file_location, prompt_precursor, name) {
+  # Read the contents of the HTML file into R
+  html_content <- readLines(file_location, warn = FALSE)
+  
+  # Concatenate the lines into a single character string
+  html_content <- paste(html_content, collapse = "\n")
+  
+  # Generate timestamp
+  timestamp <- format(Sys.time(), "%Y-%m-%d_%H-%M")
+  
+  # Create a filename based on the argument name and timestamp
+  name <- paste0(name, "_", timestamp)
+  
+  # Ask ChatGPT the specified prompt
+  response <- ask_chatgpt(paste0(prompt_precursor, 
+                                     html_content))
+  
+  # Write the HTML content to a file with the generated filename
+  # writeLines(html_content, file.path(here(), paste0("output/", file_name, ".txt")))
+  #assign(name, html_content, envir = .GlobalEnv)
+  
+  # Return the HTML content
+  return(response)
 }
